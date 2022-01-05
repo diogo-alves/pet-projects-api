eceb170e7f45b1e60fa601b4525a5a514e650c1f
Diogo Alves <diogo.alves.ti@gmail.com>
2022-01-05 09:30:44 -0300

    Setup app for deploy to Heroku

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,18 @@
+from typing import List
+
 from pydantic import BaseSettings
+from pydantic.networks import AnyHttpUrl
 
 
 class Settings(BaseSettings):
-    ACCESS_TOKEN_EXPIRATION_MINUTES: int
+    ACCESS_TOKEN_EXPIRATION_MINUTES: int = 30
     API_PREFIX: str = '/api'
     API_V1_PREFIX: str = '/v1'
     APP_NAME: str = 'Pet Projects API'
     DATABASE_URL: str
     DEFAULT_SUPERUSER_EMAIL: str
     SECRET_KEY: str
+    CORS_ALLOWED_ORIGINS: List[AnyHttpUrl] = []
 
     class Config:
         env_file = '.env'
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import register_api
 from app.core.config import settings
@@ -36,6 +37,16 @@ app = FastAPI(
         },
     ],
 )
+if settings.CORS_ALLOWED_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin) for origin in settings.CORS_ALLOWED_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.on_event('startup')
